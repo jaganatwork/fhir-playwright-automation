8038d902470f35c3a297d3dc2055e0458c10bd1c
Jagannathan Naganathan <102827754+jaganatwork@users.noreply.github.com>
2025-10-16 19:22:34 +0000

    setup eslint, prettier, husky pre-commit hook integration

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('should load Playwright website', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await expect(page).toHaveTitle(/Playwright/);
+    await page.goto('https://playwright.dev/');
+    await expect(page).toHaveTitle(/Playwright/);
 });
